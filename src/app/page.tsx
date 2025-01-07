d5831c8974241ab4cb8a5eb1829b37b1617846d6
nguyenson-255 <nguyenson10b12@gmail.com>
2025-01-07 22:42:53 +0700

    build test host

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,45 @@
-import Image from "next/image";
+'use client'
+
+import { log } from "console";
+import { useEffect, useState } from "react"
 
 export default function Home() {
+
+  const [data,setData] = useState(null);
+
+  useEffect(() => {
+    fetchData();
+  },[]);
+
+  useEffect(() => {
+    console.log(data);
+  },[data]);
+
+  const fetchData = async () =>  {
+    try {
+      const response = await fetch("https://api.flightapi.io/schedule/677d155b3a45b414acd274f7?mode=arrivals&iata=SIN&day=1");
+
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+
+      const result = await response.json();
+
+      setData(result);
+
+      console.log(data);
+      
+    } catch (error) {
+      console.log("error");
+    }
+  };
+
   return (
     <h1 className="bg-blue-700 underline">
-      Hello world!
+      ddd
     </h1>
   )
 }
+
+
+
