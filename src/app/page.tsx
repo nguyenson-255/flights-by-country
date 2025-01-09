d26d0b0b98d591b56c744a735aecdc9a31e44139
nguyenson-255 <nguyenson10b12@gmail.com>
2025-01-09 13:43:39 +0700

    update UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -95,18 +95,28 @@ export default function Home() {
   }
 
   const handelSearch = async () => {
-    fetchData((document.getElementById("default-search") as HTMLInputElement).value);
+    var code = (document.getElementById("default-search") as HTMLInputElement).value;
+
+    if (code !== '' && code !== undefined && code != null) {
+      fetchData(code);
+    } else {
+      window.alert("Enter a 3-character airport code")
+    }
+
     // fetDataLocal();
   }
 
   return (
-    <div>
+    <div className="">
+      <section className="my-4 mx-16 bg-center bg-no-repeat bg-[url('https://www.gstatic.com/travel-frontend/animation/hero/flights_nc_4.svg')] bg-white bg-blend-multiply">
       <div className="py-8 px-4 mx-auto max-w-md text-center lg:py-16 lg:max-w-2xl" >
-        <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+        <h1 className="mb-4 text-3xl font-extrabold tracking-tight leading-none text-gray-700 md:text-4xl lg:text-5xl dark:text-white">
           Flights by Country
         </h1>
       </div>
-      <div className="mx-auto max-w-md lg:max-w-2xl">
+      </section>
+
+      <div className="mx-auto max-w-2xl lg:max-w-2xl">
         <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
         <div className="relative">
           <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
@@ -114,7 +124,7 @@ export default function Home() {
               <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
             </svg>
           </div>
-          <input type="search" id="default-search" className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Code..." required />
+          <input type="search" id="default-search" className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter a 3-character airport code..." required />
 
           <button type="submit" className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={handelSearch}>Search</button>
         </div>
